612d8ec180e2d9183bd583378520f98676cef919
tommypacker <tommypacker@gmail.com>
2015-12-24 11:34:02 -0800

    Added basic info to "role" details

--- BearTracksIOS/RoleDetailViewController.swift
+++ BearTracksIOS/RoleDetailViewController.swift
@@ -12,7 +12,11 @@ class RoleDetailViewController: UIViewController {
     
     //MARK: Properties
     @IBOutlet weak var roleTitle: UILabel!
+    @IBOutlet weak var creatorLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     
+    let peopleRef = Firebase(url: "https://beartracks.firebaseio.com/people")
     var firebaseKey = ""
     
     override func viewDidLoad() {
@@ -31,6 +35,34 @@ class RoleDetailViewController: UIViewController {
     func loadRoleDetails(ref: Firebase){
         ref.observeEventType(.Value, withBlock: { snapshot in
             self.roleTitle.text = snapshot.value["name"] as? String
+            let creatorKey = snapshot.value["creator"] as? String
+            self.findCreator(creatorKey!)
+            let ownerKey = snapshot.value["owner"] as? String
+            if(ownerKey != nil){
+                self.findOwner(ownerKey!)
+            }else{
+                self.ownerLabel.text = "No owner"
+            }
+            let status = snapshot.value["status"] as? String
+            self.statusLabel.text = "Status:  " + status!
+        })
+    }
+    
+    func findCreator(key: String){
+        let creatorRef = peopleRef.childByAppendingPath(key)
+        var personName = "Team766 Member"
+        creatorRef.observeEventType(.Value, withBlock: {snapshot in
+            personName = (snapshot.value["name"] as? String)!
+            self.creatorLabel.text = "Creator:  " + personName
+        })
+    }
+    
+    func findOwner(key: String){
+        let ownerRef = peopleRef.childByAppendingPath(key)
+        var ownerName = "Team 766 Member"
+        ownerRef.observeEventType(.Value, withBlock: {snapshot in
+            ownerName = (snapshot.value["name"] as? String)!
+            self.ownerLabel.text = "Owner:  " + ownerName
         })
     }
     
